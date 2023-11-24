39c433d929a0805fe2fcdb82063dd1e2bd46ef0c
NelyR55 <129916803+NelyR55@users.noreply.github.com>
2023-11-23 19:29:30 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 var bandera =false; 
+
 function validar (){
  if(bandera){
      console.log("Validacion completa");
+  //redirecciona a la pagina correspondiente
  }
 }
-function correctCaptcha (){
+function correctCaptcha(){
 
      console.log("Captcha resuelto correctamente");
      bandera = true;
